5a91c907ad5c3b6add33259b0582c858265e8f9d
Tomas Fagerbekk <tomas@otovo.com>
2019-12-23 23:22:45 +0100

    Updates

---
+++ precache-manifest.a1cff0f90e839d5561ce3072d661e1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba635a0826a0d418013875cc0d014091",
+    "url": "/hanna-19/index.html"
+  },
+  {
+    "revision": "ed71271e064da4502003",
+    "url": "/hanna-19/static/css/main.bfd62860.chunk.css"
+  },
+  {
+    "revision": "2d202d4615f830b2a631",
+    "url": "/hanna-19/static/js/2.8f4a9693.chunk.js"
+  },
+  {
+    "revision": "47697e52d12ae7ec50cc0b479ad94368",
+    "url": "/hanna-19/static/js/2.8f4a9693.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed71271e064da4502003",
+    "url": "/hanna-19/static/js/main.face7e02.chunk.js"
+  },
+  {
+    "revision": "bb01577b788b9def69ef",
+    "url": "/hanna-19/static/js/runtime-main.821db44f.js"
+  }
+]);
