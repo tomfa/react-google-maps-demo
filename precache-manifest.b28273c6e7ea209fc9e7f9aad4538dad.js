d5d7098825ed696507d75b4b7573d7db87239444
Tomas Fagerbekk <tomas@otovo.com>
2019-12-24 01:42:09 +0100

    Updates

---
+++ precache-manifest.b28273c6e7ea209fc9e7f9aad4538dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838b56462c3d2bfd72d7515779ab621e",
+    "url": "/react-google-maps-demo/index.html"
+  },
+  {
+    "revision": "3c8662be14a1e34b3c4b",
+    "url": "/react-google-maps-demo/static/css/main.bfd62860.chunk.css"
+  },
+  {
+    "revision": "c53465430588fd7e426c",
+    "url": "/react-google-maps-demo/static/js/2.e6e6b7d1.chunk.js"
+  },
+  {
+    "revision": "47697e52d12ae7ec50cc0b479ad94368",
+    "url": "/react-google-maps-demo/static/js/2.e6e6b7d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c8662be14a1e34b3c4b",
+    "url": "/react-google-maps-demo/static/js/main.c1736291.chunk.js"
+  },
+  {
+    "revision": "177fe0b16ca1611398af",
+    "url": "/react-google-maps-demo/static/js/runtime-main.28aff570.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hanna-19/precache-manifest.968c1bb39dbf0b426317e512671169cb.js"
+  "/react-google-maps-demo/precache-manifest.b28273c6e7ea209fc9e7f9aad4538dad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hanna-19/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-google-maps-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
